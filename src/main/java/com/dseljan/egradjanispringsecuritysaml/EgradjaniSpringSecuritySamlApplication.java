524ab9c79d6e8eb36d2ec95857b85f39e1d96986
dseljan@tvz.hr <dseljan@tvz.hr>
2022-09-21 22:17:20 +0200

    Initial commit

---
+++ src/main/java/com/dseljan/egradjanispringsecuritysaml/EgradjaniSpringSecuritySamlApplication.java
@@ -0,0 +1,13 @@
+package com.dseljan.egradjanispringsecuritysaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgradjaniSpringSecuritySamlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgradjaniSpringSecuritySamlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dseljan/egradjanispringsecuritysaml/EgradjaniSpringSecuritySamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dseljan.egradjanispringsecuritysaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgradjaniSpringSecuritySamlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
